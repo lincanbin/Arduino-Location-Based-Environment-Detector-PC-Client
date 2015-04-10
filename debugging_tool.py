e218ea9967d8911f17fda8d03442f6526c3f08a3
lincanbin <lincanbin@hotmail.com>
2015-04-11 00:27:13 +0800

    Flush buffer of COM3.

--- debugging_tool.py
+++ debugging_tool.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python
 # -*- coding=utf-8 -*-
 import time, serial, urllib, urllib2
+# pyserial is required. 
+# http://pyserial.sourceforge.net/
 print 'start\n'
 try:
     ser = serial.Serial(2)    # open COM3
     ser.baudrate = 9600
-    for x in xrange(1,100):
-        print x,
+    for x in xrange(1,10000):
+        print str(x) + ':  ' + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + ': '
         line = ser.readline().replace('\r\n', '').split(',')
-        print line
         data = {
             'device_index': 13726247339,
             'longitude': 113.540718,
@@ -17,14 +18,15 @@ try:
             'humidity': line[2],
             'particulate_matter': line[0]
         }
+        print data
         url = 'http://monitor.ourjnu.com/submit.php?'+urllib.urlencode(data)
         try:
             response = urllib2.urlopen(url).read()
-            print response
-            time.sleep(3)    #sleep 3 seconds
+            print response + '\n\n'
+            #time.sleep(60)    #sleep 60 seconds
         except:
-            print 'Network error.'
-        ser.flush()
+            print 'Network error.\n\n'
+        ser.flushOutput()
     ser.close()
 except:
     print 'Could not open port COM3.'
